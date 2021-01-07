27c5d7e88f7fbb01a0e18ac40fce16ccd87574cb
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2021-01-07 08:35:38 +0530

    Fixes

--- amebot/__main__.py
+++ amebot/__main__.py
@@ -1,13 +1,13 @@
-from userbot import ALIVE_PIC
-from userbot import bot
+from amebot import ALIVE_PIC
+from amebot import bot
 from sys import argv
 import sys
 from telethon.errors.rpcerrorlist import PhoneNumberInvalidError
 import os
 from telethon import TelegramClient
 from var import Var
-from userbot.utils import load_module
-from userbot import LOAD_PLUG, BOTLOG_CHATID, LOGS
+from amebot.utils import load_module
+from amebot import LOAD_PLUG, BOTLOG_CHATID, LOGS
 from pathlib import Path
 import asyncio
 import telethon.utils
@@ -30,7 +30,7 @@ else:
             api_id=Var.APP_ID,
             api_hash=Var.API_HASH
         ).start(bot_token=Var.TG_BOT_TOKEN_BF_HER)
-        print("Initialisation finished with no errors")
+        print("Work Done with no errors")
         print("Starting Amebot")
         bot.loop.run_until_complete(add_bot(Var.TG_BOT_USER_NAME_BF_HER))
         print("Startup Completed")
@@ -38,22 +38,24 @@ else:
         bot.start()
     
 import glob
-path = 'amebot/plugins/*.py'
-files = glob.glob(path)
+pach = 'amebot/plugins/*.py'
+files = glob.glob(pach)
 for name in files:
     with open(name) as f:
-        path1 = Path(f.name)
-        shortname = path1.stem
+        pach1 = Pach(f.name)
+        shortname = pach1.stem
         load_module(shortname.replace(".py", ""))
         
 if ALIVE_PIC is not None:
-    print("###--------------Alive Pic Added Successfully--------------###")
-    
-
-
-import userbot._core
-print("Everything is Alright, Do .alive or .help to Check Online Status of Your Bot !!")
-print("Join @marshmellowsupport For Reporting any Bug or an Issue !!")
+    print("""╔═══╗─────╔╗────╔╗
+             ║╔═╗║─────║║───╔╝╚╗
+             ║║─║╠╗╔╦══╣╚═╦═╩╗╔╝
+             ║╚═╝║╚╝║║═╣╔╗║╔╗║║
+             ║╔═╗║║║║║═╣╚╝║╚╝║╚╗
+             ╚╝─╚╩╩╩╩══╩══╩══╩═╝""")
+import amebot._core
+print("Bot is ready for helping u try it wiyh `.alive` or `.help`!!")
+print("Join `@Amebotsupport` we are always there to help you !!")
 
 
 if len(argv) not in (1, 3, 4):
