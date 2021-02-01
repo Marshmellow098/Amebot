e08c29d384d41d99ed8d95e273c8c3f741fa8cc7
Marshmellow098 <74300487+Marshmellow098@users.noreply.github.com>
2021-02-01 19:00:08 +0530

    cmd

--- amebot/plugins/userinfo.py
+++ amebot/plugins/userinfo.py
@@ -6,11 +6,11 @@ from telethon.tl.functions.photos import GetUserPhotosRequest
 from telethon.tl.functions.users import GetFullUserRequest
 from telethon.tl.types import MessageEntityMentionName
 from telethon.utils import get_input_location
-from userbot.utils import amebot_mcd
+from userbot.utils import amebot_cmd
 
 
 
-@borg.on(amebot_mcd("info ?(.*)"))
+@borg.on(amebot_cmd("info ?(.*)"))
 async def _(event):
     if event.fwd_from:
         return
